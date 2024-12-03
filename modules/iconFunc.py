dd305ad82129bfa6ccb055509817653dc82dcea0
serii burduja <seriiburduja@gmail.com>
2024-12-03 11:35:18 +0200

    feat: icons

--- modules/iconFunc.py
+++ modules/iconFunc.py
@@ -1,16 +1,21 @@
 import os
 import subprocess
+from rich import print
 from utils.createFile import createFile
-from utils.getLayoutPath import getLayoutPath
-from utils.layoutToFile import layoutToFile
+from utils.getFromClipboard import getFromClipBoard
 
 def iconFunc():
+    svg_content = getFromClipBoard()
+    if not '<svg' in svg_content:
+        print("[red]This is not an svg content")
+        return
+    svg_content = f"<template>\n{svg_content}\n</template>"
     dir_path = 'src/icons'
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     file_path = createFile(dir_path, 'vue')
     print(f"file_path: {file_path}")
-    layout_path = getLayoutPath('vue')
-    layoutToFile(layout_path, file_path)
+    with open(file_path, 'w') as f:
+        f.write(svg_content)
     subprocess.run(["bat", file_path], check=True)
 
---
+++ utils/getFromClipboard.py
@@ -0,0 +1,6 @@
+import os
+
+def getFromClipBoard():
+    command = 'xclip -o -selection clipboard'
+    print(f"command from buffer: {command}")
+    return os.popen(command).read().strip()
