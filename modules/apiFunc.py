90fda0f4396f15e60818339f14d302264f5d5364
serii burduja <seriiburduja@gmail.com>
2024-12-07 13:08:24 +0200

    feat: api

--- modules/apiFunc.py
+++ modules/apiFunc.py
@@ -1,11 +1,15 @@
 import subprocess
 from utils.createDir import createDir
 from utils.createFile import createFile
+from utils.getConfigData import getConfigData
 from utils.getLayoutPath import getLayoutPath
+from utils.getSelectedTemplate import getSelectedTemplate
 from utils.layoutToFile import layoutToFile
 
 def apiFunc():
-    dir_path = createDir('src/api')
+    config_txt = getSelectedTemplate()
+    is_vue = True if config_txt == 'vue' else False
+    dir_path = getConfigData(is_vue, path='api')
     print(f"dir_path: {dir_path}")
     file_path = createFile(dir_path, 'ts')
     print(f"file_path: {file_path}")
