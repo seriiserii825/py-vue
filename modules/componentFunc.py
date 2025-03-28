235856914895ca2f83cc5f20d48a42e248a4bd40
serii burduja <seriiburduja@gmail.com>
2025-03-28 14:54:47 +0200

    feat: component func with dir path

--- modules/componentFunc.py
+++ modules/componentFunc.py
@@ -9,14 +9,13 @@ from utils.getSelectedTemplate import getSelectedTemplate
 def componentFunc():
     config_txt = getSelectedTemplate()
     dir_path = getConfigData(config_txt, path='components')
-    print(f"dir_path: {dir_path}")
+    dir_name = chooseOrCreateDirectory(dir_path)
+    dir_path = f"{dir_path}/{dir_name}"
     file_path = createFile(dir_path, 'vue')
     print(f"file_path: {file_path}")
     Layout('vue', file_path)
-    dir_name = chooseOrCreateDirectory(dir_path)
-    print(f'dir_name: {dir_name}')
-    # file_name = file_path.split('/')[-1].split('.')[0]
-    # class_name = camelToKebabCase(file_name)
-    # subprocess.run(["sed", "-i", f"s|vue|{class_name}|g", file_path], check=True)
-    # subprocess.run(["bat", file_path], check=True)
-    #
+    file_name = file_path.split('/')[-1].split('.')[0]
+    class_name = camelToKebabCase(file_name)
+    subprocess.run(["sed", "-i", f"s|vue|{class_name}|g", file_path], check=True)
+    subprocess.run(["bat", file_path], check=True)
+
