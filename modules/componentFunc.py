febe1c08de186cb660e91d3e2b98dfdbc885ebfc
serii burduja <seriiburduja@gmail.com>
2025-06-13 18:50:56 +0300

    feat: file

--- modules/componentFunc.py
+++ modules/componentFunc.py
@@ -6,16 +6,19 @@ from utils.createFile import createFile
 from utils.getConfigData import getConfigData
 from utils.getSelectedTemplate import getSelectedTemplate
 
+
 def componentFunc():
     config_txt = getSelectedTemplate()
     dir_path = getConfigData(config_txt, path='components')
-    dir_name = chooseOrCreateDirectory(dir_path)
-    dir_path = f"{dir_path}/{dir_name}"
+    need_a_dir = input("Do you need a directory? (y/n): ").strip().lower()
+    if need_a_dir == 'y':
+        dir_name = chooseOrCreateDirectory(dir_path)
+        dir_path = f"{dir_path}/{dir_name}"
     file_path = createFile(dir_path, 'vue')
     print(f"file_path: {file_path}")
     Layout('vue', file_path)
     file_name = file_path.split('/')[-1].split('.')[0]
     class_name = camelToKebabCase(file_name)
-    subprocess.run(["sed", "-i", f"s|vue|{class_name}|g", file_path], check=True)
+    subprocess.run(
+        ["sed", "-i", f"s|vue|{class_name}|g", file_path], check=True)
     subprocess.run(["bat", file_path], check=True)
-
