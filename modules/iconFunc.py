67ff2c160d5dec044ca6fe9cf5ce930aa6ec7fa7
serii burduja <seriiburduja@gmail.com>
2026-04-30 18:40:39 +0300

    feat: fix icon func

--- modules/iconFunc.py
+++ modules/iconFunc.py
@@ -32,10 +32,9 @@ def create_file_name_from_path(file_path):
     # // get last 2 segments of the file path without extension
     segments = file_path.replace("\\", "/").split("/")
     last_two_segments = segments[-2:]
-    segments_without_extension = [os.path.splitext(
-        seg)[0] for seg in last_two_segments]
+    segments_without_extension = [os.path.splitext(seg)[0] for seg in last_two_segments]
     # join the segments without delimiter
-    file_name = "".join(segments_without_extension)
+    file_name = segments_without_extension[-1]
     file_to_copy = f"<{file_name} />"
     ClipboardManager.write(file_to_copy)
     Notification(file_to_copy, f"Copied to clipboard: {file_to_copy}").notify()
